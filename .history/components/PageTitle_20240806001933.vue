be73eb32e8767a6e7732321279c1a725b5bca771
Pavel1Uvarov <pavel2uvaorv@gmail.com>
2024-08-06 00:22:38 +0200

    feat: wip

--- .history/components/PageTitle_20240806001933.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h1 class="font-poppins font-bold text-5xl text-primary leading-12">
-    <slot />
-  </h1>
-</template>
--- .history/types/card.interface_20240805234441.ts
+++
@@ -1,7 +0,0 @@
-export interface ICard {
-  uuid: string;
-  title: string;
-  list?: string[],
-  description: string[];
-  images: string[];
-}
